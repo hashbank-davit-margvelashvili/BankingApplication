e8e1d365a23f481f01cf79196006ee5d2d16adb7
dmargvelashvili <davit.margvelashvili@hashbank.ge>
2026-03-31 13:12:51 +0400

    Excercise 1 completed from Lesson-3

---
+++ BankingApplication.Common.Tests/HspFunctionsTests.cs
@@ -0,0 +1,34 @@
+﻿namespace BankingApplication.Common.Tests;
+
+public class HspFunctionsTests
+{
+    [Theory]
+    [InlineData("UserErrorClass60000", 60000)]
+    [InlineData("SysErrorClass60001", 60001)]
+    [InlineData("ARC_GeneralError101", 101)]
+    [InlineData("SomethingElse", 60000)]
+    [InlineData("", 60000)]
+    [InlineData("unknown_type", 60000)]
+    [InlineData(null, 60000)]
+    public void GetErrorNumberConstant_ReturnsCorrectValue(string input, int expectedResult)
+    {
+        // Act
+        var actualResult = HspFunctions.GetErrorNumberConstant(input);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void GetErrorNumberConstant_ReturnsCorrectValue_IfParameterOmitted()
+    {
+        // Arrange
+        var expectedResult = 60000;
+
+        // Act
+        var actualResult = HspFunctions.GetErrorNumberConstant();
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ BankingApplication.Common/HspFunctions.cs
@@ -0,0 +1,41 @@
+﻿namespace BankingApplication.Common;
+
+/*
+    -- Source: HSP/hp/Functions/fnGetErrorNumberConstant.sql
+    CREATE FUNCTION hp.[fnGetErrorNumberConstant]
+    (
+        @Type VARCHAR(50) = 'UserErrorClass60000'
+    )
+    RETURNS INT
+    AS
+        BEGIN
+    RETURN(
+        CASE
+    WHEN @Type = 'UserErrorClass60000' THEN 60000
+    WHEN @Type = 'SysErrorClass60001' THEN 60001
+    WHEN @Type = 'ARC_GeneralError101' THEN 101
+    ELSE 60000 END
+        )
+    END
+*/
+
+public static class HspFunctions
+{
+    public static int GetErrorNumberConstant(string type = "UserErrorClass60000")
+    {
+        switch (type)
+        {
+            case "UserErrorClass60000":
+                return 60000;
+
+            case "SysErrorClass60001":
+                return 60001;
+
+            case "ARC_GeneralError101":
+                return 101;
+
+            default:
+                return 60000;
+        }
+    }
+}
